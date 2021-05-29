ba7b6ead6040aeec935454dec93873f9e3d614c2
1KVueltasAlCampo <angrybirdsgallo@gmail.com>
2021-05-29 04:00:37 -0500

    Adjustments for vehicle classes

--- src/model/Motorcycle.java
+++ src/model/Motorcycle.java
@@ -2,11 +2,11 @@ package model;
 
 public class Motorcycle extends Vehicle{
     private final static int HOURVALUE = 1000;
-    private final static int ADITIONAL = 500;
-    private final static int DAY = 3000;
-    private final static int NIGHT = 3000;
-    private final static int DAYANDNIGHT = 4000;
-    private final static int MONTH = 30000;
+    private final static int ADITIONALVALUE = 500;
+    private final static int DAYVALUE = 3000;
+    private final static int NIGHTVALUE = 3000;
+    private final static int DAYANDNIGHTVALUE = 4000;
+    private final static int MONTHVALUE = 30000;
     private final static int[] availableSpots = {-9,-8,-7,-6,-5,-4,-3,-2,-1};
 
     public Motorcycle(int typeIndicator, String model, String licensePlate, String color, Client owner, int spot, String observations, int stayIndicator, int numberOfTime) {
--- src/model/SmallVehicle.java
+++ src/model/SmallVehicle.java
@@ -3,11 +3,11 @@ package model;
 public class SmallVehicle extends Vehicle{
     private final static int HOURVALUE = 2000;
     private final static int TWOHOURSVALUE = 3000;
-    private final static int ADITIONAL = 500;
-    private final static int DAY = 6000;
-    private final static int NIGHT = 6000;
-    private final static int DAYANDNIGHT = 8000;
-    private final static int MONTH = 55000;
+    private final static int ADITIONALVALUE = 500;
+    private final static int DAYVALUE = 6000;
+    private final static int NIGHTVALUE = 6000;
+    private final static int DAYANDNIGHTVALUE = 8000;
+    private final static int MONTHVALUE = 55000;
     private final static int[] availableSpots = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
 
     public SmallVehicle(int typeIndicator, String model, String licensePlate, String color, Client owner, int spot, String observations, int stayIndicator, int numberOfTime) {
