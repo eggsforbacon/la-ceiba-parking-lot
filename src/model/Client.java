c920d636ac487fd8669495ea841df57979fd8ac4
Sebastián Pérez <sebas8041@hotmail.com>
2021-05-30 00:04:58 -0500

    Methods added

--- src/model/Client.java
+++ src/model/Client.java
@@ -1,8 +1,24 @@
 package model;
 
 public class Client extends Person {
-
-    public Client(String name, String id) {
+	
+	private String cellNumber;
+	private boolean state;
+	
+    public Client(String name, String id, String cellNumber) {
         super(name, id);
+        this.cellNumber=cellNumber;
+        state=true;
     }
+    
+
+	public String getCellNumber() {
+		return cellNumber;
+	}
+
+	public void setCellNumber(String cellNumber) {
+		this.cellNumber = cellNumber;
+	} 
+    
+    
 }
--- src/model/ParkingLot.java
+++ src/model/ParkingLot.java
@@ -6,9 +6,27 @@ import java.util.ArrayList;
 public class ParkingLot implements Serializable {
     private ArrayList<Client> clientsPL;
     private ArrayList<Vehicle> vehiclesPL;
+  
 
     public ParkingLot(){
         clientsPL = new ArrayList<>();
         vehiclesPL = new ArrayList<>();
     }
+    
+    public String ClientVeryfier(String name, String id, String number) {
+    	String temp="";
+	    	if(clientsPL!=null) {
+	    		Client aux=new Client(name, id, number);
+		    	for(int i=0;i<clientsPL.size();i++) {		    		
+		    		if (aux.getName().equalsIgnoreCase(clientsPL.get(i).getName())) {
+		    			temp+="name";
+		    		}
+		    		if(aux.getId().equalsIgnoreCase(clientsPL.get(i).getId())) {
+		    			temp+="id";
+		    		}
+		    	}
+	    	}
+	    	 return temp;
+    	}
+   
 }
